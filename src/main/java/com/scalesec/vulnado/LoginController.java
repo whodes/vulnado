e76ef9ef22809beb6e888f20ebda89c3fb934564
pixeebot <help@pixee.ai>
2024-01-30 17:21:11 +0000

    Replaced @RequestMapping annotation with shortcut annotation for requested HTTP Method

--- src/main/java/com/scalesec/vulnado/LoginController.java
+++ src/main/java/com/scalesec/vulnado/LoginController.java
@@ -15,7 +15,7 @@ public class LoginController {
   private String secret;
 
   @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+  @PostMapping(value = "/login", produces = "application/json", consumes = "application/json")
   LoginResponse login(@RequestBody LoginRequest input) {
     User user = User.fetch(input.username);
     if (Postgres.md5(input.password).equals(user.hashedPassword)) {
